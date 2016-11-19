5d3802917de3f81786d8babdaf35a76151e236bd
Michal Pavlusek <pavlusek@datalite.cz>
2016-11-19 20:29:43 +0100

    Remove ExtJs build directory

--- ext/build/temp/production/JavaDbAdmin/sass/config.rb
+++
@@ -1,2 +0,0 @@
-Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
-cache_path = 'C:\Users\Michal\Projekty\javaDbAdmin\ext/build/.sass-cache'
--- ext/build/temp/production/JavaDbAdmin/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require C:\Users\Michal\Projekty\javaDbAdmin\ext\app.js
