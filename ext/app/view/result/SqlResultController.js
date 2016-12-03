bca46670ca51cd36085afce5799399f623422235
Michal Pavlusek <pavlusek@datalite.cz>
2016-12-03 23:08:17 +0100

    Displaying SQL query result in southern tab.

--- ext/app/view/result/SqlResultController.js
+++ ext/app/view/result/SqlResultController.js
@@ -30,6 +30,66 @@ Ext.define('JavaDbAdmin.view.connect.SqlResultController', {
     {
         JavaDbAdmin.view.connect.SqlResultController.activateResultTab();
 
-        // TODO dynamic model and store
+        if(data.length > 0)
+        {
+            var model = this.createSqlResultModel(data[0]);
+            var store = this.createSqlResultStore(model, data);
+            this.getView().setColumns(this.createSqlResultColumns(data[0]));
+            this.getView().setStore(store);
+        }
+    },
+
+    /**
+     * Create model of current sql result.
+     * @param firstItem of result.
+     * @returns {Ext.data.Model}
+     */
+    createSqlResultModel: function(firstItem)
+    {
+        var fieldsModel = [];
+
+        //create dynamic fields
+        for (key in firstItem)
+        {
+            var fieldModel = {};
+            fieldModel.mapping = fieldModel.name = key;
+            fieldsModel.push(fieldModel);
+        }
+
+        //create model
+        return Ext.create('Ext.data.Model', {
+            fields: fieldsModel
+        });
+    },
+
+    /**
+     * Create store of current model and sql result.
+     * @returns {Ext.data.Store}
+     */
+    createSqlResultStore: function(model, data)
+    {
+        return Ext.create('Ext.data.Store', {
+            model: model,
+            data: data
+        });
+    },
+
+    /**
+     * Create columns model of current sql result.
+     * @param firstItem of result.
+     * @returns {Array}
+     */
+    createSqlResultColumns: function(firstItem)
+    {
+        var columnsModel = [];
+
+        //create dynamic columns
+        for (key in firstItem)
+        {
+            var columnModel = {};
+            columnModel.text = columnModel.dataIndex = key;
+            columnsModel.push(columnModel);
+        }
+        return columnsModel;
     }
 });
--- ext/classic/src/view/result/ResultList.js
+++ ext/classic/src/view/result/ResultList.js
@@ -2,10 +2,8 @@
  * Result view for the application. View showing result of sql queries to user.
  */
 Ext.define('JavaDbAdmin.view.result.ResultList', {
-    extend: 'Ext.panel.Panel',
+    extend: 'Ext.grid.Panel',
     xtype: 'resultList',
 
-    controller: 'sqlresult',
-
-    html: 'result panel'
+    controller: 'sqlresult'
 });
